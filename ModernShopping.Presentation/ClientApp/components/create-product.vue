0f43e4349194ef38d970091f3257a13b25222d12
phu.nguyentan <phu.nguyentan@harveynash.vn>
2019-01-08 00:02:14 +0700

    Add remove, add button in create product

--- ModernShopping.Presentation/ClientApp/components/create-product.vue
+++ ModernShopping.Presentation/ClientApp/components/create-product.vue
@@ -1,113 +1,149 @@
 <template>
     <form :class="{'was-validated': isSubmited}" novalidate @submit.prevent="createProducts">
         <div class="row justify-content-center">
-            <div class="col-8" v-for="(product,index) in productsToCreate" :key="index">
-                <div class="card mb-3">
-                    <div class="card-header bg-info">Product #{{ index + 1 }}</div>
-                    <div class="card-body">
-                        <div class="form-row mb-3">
-                            <div class="col-6">
-                                <label for="product-name">Product name</label>
-                                <input
-                                    type="text"
-                                    class="form-control form-control-lg"
-                                    id="product-name"
-                                    placeholder="Sample name"
-                                    required
-                                    v-model.trim="product.productName"
-                                >
-                                <div class="valid-feedback">Looks good!</div>
-                                <div class="invalid-feedback">Please provide product name.</div>
-                            </div>
-                            <div class="col-6">
-                                <label for="quantity-per-unit">Quanlity per unit</label>
-                                <input
-                                    type="text"
-                                    class="form-control form-control-lg"
-                                    id="quantity-per-unit"
-                                    placeholder="e.g. 10 boxes x 20 bags"
-                                    required
-                                    v-model.trim="product.quanlityPerUnit"
-                                >
-                                <div class="valid-feedback">Looks good!</div>
-                                <div class="invalid-feedback">Please provide quanlity per unit.</div>
-                            </div>
-                        </div>
+            <div class="col-8 mb-3">
+                <button
+                    type="button"
+                    class="btn btn-secondary"
+                    v-if="productsToCreate.length === 0"
+                    @click="addProductToCreate"
+                >
+                    <font-awesome-icon icon="plus"/>&nbsp;Add more product
+                </button>
+            </div>
 
-                        <div class="form-row mb-3">
-                            <div class="col-6">
-                                <label for="product-name">Category</label>
-                                <base-select2
-                                    url="/api/data-source/category-source/"
-                                    :min-input-length="2"
-                                    v-model="product.category"
-                                    :required="true"
-                                />
-                                <div class="valid-feedback">Looks good!</div>
-                                <div class="invalid-feedback">Please select a category.</div>
-                            </div>
-                            <div class="col-6">
-                                <label for="quantity-per-unit">Supplier</label>
-                                <base-select2
-                                    url="/api/data-source/supplier-source/"
-                                    :min-input-length="2"
-                                    v-model="product.supplier"
-                                    :required="true"
-                                />
-                                <div class="valid-feedback">Looks good!</div>
-                                <div class="invalid-feedback">Please select a supplier.</div>
-                            </div>
+            <transition-group
+                tag="div"
+                name="list"
+                enter-active-class="animated slideInLeft"
+                leave-active-class="animated slideOutRight"
+                class="col-8"
+            >
+                <div v-for="(product,index) in productsToCreate" :key="index">
+                    <div class="card mb-3">
+                        <div class="card-header bg-info">
+                            Product #{{ index + 1 }}
+                            <button
+                                type="button"
+                                class="btn btn-secondary"
+                                @click="removeProductToCreate(index)"
+                            >
+                                <font-awesome-icon icon="minus"/>
+                            </button>
+                            <button
+                                type="button"
+                                class="btn btn-light float-right"
+                                v-if="index === productsToCreate.length - 1"
+                                @click="addProductToCreate"
+                            >
+                                <font-awesome-icon icon="plus"/>
+                            </button>
                         </div>
-
-                        <div class="form-row mb-3">
-                            <div class="col-4">
-                                <label for="unit-price">Unit price</label>
-                                <div class="input-group mb-3">
-                                    <div class="input-group-prepend">
-                                        <span class="input-group-text">$</span>
-                                    </div>
+                        <div class="card-body">
+                            <div class="form-row mb-3">
+                                <div class="col-6">
+                                    <label for="product-name">Product name</label>
                                     <input
-                                        type="number"
-                                        id="unit-price"
-                                        class="form-control"
+                                        type="text"
+                                        class="form-control form-control-lg"
+                                        id="product-name"
+                                        placeholder="Sample name"
                                         required
-                                        v-model="product.unitPrice"
+                                        v-model.trim="product.productName"
                                     >
                                     <div class="valid-feedback">Looks good!</div>
-                                    <div class="invalid-feedback">Please provide unit price.</div>
+                                    <div class="invalid-feedback">Please provide product name.</div>
+                                </div>
+                                <div class="col-6">
+                                    <label for="quantity-per-unit">Quanlity per unit</label>
+                                    <input
+                                        type="text"
+                                        class="form-control form-control-lg"
+                                        id="quantity-per-unit"
+                                        placeholder="e.g. 10 boxes x 20 bags"
+                                        required
+                                        v-model.trim="product.quanlityPerUnit"
+                                    >
+                                    <div class="valid-feedback">Looks good!</div>
+                                    <div class="invalid-feedback">Please provide quanlity per unit.</div>
                                 </div>
                             </div>
-                            <div class="col-4">
-                                <label for="quantity-per-unit">Units in stock</label>
-                                <input
-                                    type="number"
-                                    id="units-in-stocks"
-                                    class="form-control"
-                                    required
-                                    v-model="product.unitsInStock"
-                                >
-                                <div class="valid-feedback">Looks good!</div>
-                                <div class="invalid-feedback">Please provide unit in stock.</div>
+
+                            <div class="form-row mb-3">
+                                <div class="col-6">
+                                    <label for="product-name">Category</label>
+                                    <base-select2
+                                        url="/api/data-source/category-source/"
+                                        :min-input-length="2"
+                                        v-model="product.category"
+                                        :required="true"
+                                    />
+                                    <div class="valid-feedback">Looks good!</div>
+                                    <div class="invalid-feedback">Please select a category.</div>
+                                </div>
+                                <div class="col-6">
+                                    <label for="quantity-per-unit">Supplier</label>
+                                    <base-select2
+                                        url="/api/data-source/supplier-source/"
+                                        :min-input-length="2"
+                                        v-model="product.supplier"
+                                        :required="true"
+                                    />
+                                    <div class="valid-feedback">Looks good!</div>
+                                    <div class="invalid-feedback">Please select a supplier.</div>
+                                </div>
                             </div>
-                            <div class="col-4">
-                                <label for="quantity-per-unit">Units on order</label>
-                                <input
-                                    type="number"
-                                    id="units-on-order"
-                                    class="form-control"
-                                    v-model="product.unitsOnOrder"
-                                >
-                                <div class="valid-feedback">Looks good!</div>
+
+                            <div class="form-row mb-3">
+                                <div class="col-4">
+                                    <label for="unit-price">Unit price</label>
+                                    <div class="input-group mb-3">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text">$</span>
+                                        </div>
+                                        <input
+                                            type="number"
+                                            id="unit-price"
+                                            class="form-control"
+                                            required
+                                            v-model="product.unitPrice"
+                                        >
+                                        <div class="valid-feedback">Looks good!</div>
+                                        <div class="invalid-feedback">Please provide unit price.</div>
+                                    </div>
+                                </div>
+                                <div class="col-4">
+                                    <label for="quantity-per-unit">Units in stock</label>
+                                    <input
+                                        type="number"
+                                        id="units-in-stocks"
+                                        class="form-control"
+                                        required
+                                        v-model="product.unitsInStock"
+                                    >
+                                    <div class="valid-feedback">Looks good!</div>
+                                    <div class="invalid-feedback">Please provide unit in stock.</div>
+                                </div>
+                                <div class="col-4">
+                                    <label for="quantity-per-unit">Units on order</label>
+                                    <input
+                                        type="number"
+                                        id="units-on-order"
+                                        class="form-control"
+                                        v-model="product.unitsOnOrder"
+                                    >
+                                    <div class="valid-feedback">Looks good!</div>
+                                </div>
                             </div>
-                        </div>
-                        <div class="form-row mb-3">
-                            <base-image-uploader
-                                @uploadSuccess="uploadSuccessHandler(index, $event)"
-                            />
+                            <div class="form-row mb-3">
+                                <base-image-uploader
+                                    @uploadSuccess="uploadSuccessHandler(index, $event)"
+                                />
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            </transition-group>
             <div class="col-8">
                 <button class="btn btn-primary" type="submit">Submit form</button>
             </div>
@@ -132,16 +168,6 @@ export default {
                     unitsInStock: null,
                     unitsOnOrder: null,
                     imageId: null
-                },
-                {
-                    productName: null,
-                    quanlityPerUnit: null,
-                    category: null,
-                    supplier: null,
-                    unitPrice: null,
-                    unitsInStock: null,
-                    unitsOnOrder: null,
-                    imageId: null
                 }
             ]
         }
@@ -151,8 +177,38 @@ export default {
             this.isSubmited = true
             console.log(this)
         },
+        addProductToCreate() {
+            this.productsToCreate.push({
+                productName: null,
+                quanlityPerUnit: null,
+                category: null,
+                supplier: null,
+                unitPrice: null,
+                unitsInStock: null,
+                unitsOnOrder: null,
+                imageId: null
+            })
+            this.scrollToBottom()
+        },
+        removeProductToCreate(index) {
+            this.productsToCreate.splice(index, 1)
+        },
         uploadSuccessHandler(index, eventData) {
             this.productsToCreate[index].imageId = eventData.imageId
+        }
+    },
+    created() {
+        this.scrollToBottom = () => {
+            var scrollingElement = (document.scrollingElement || document.body);
+
+            const productHeight = 730
+            let scrollDistance = 0
+            const scrollInterval = setInterval(() => {
+                scrollDistance += 5
+                scrollingElement.scrollTop += 5;
+                if (scrollDistance >= productHeight)
+                    clearInterval(scrollInterval)
+            }, 10)
         }
     },
     components: {
--- ModernShopping.Presentation/ClientApp/icons.js
+++ ModernShopping.Presentation/ClientApp/icons.js
@@ -1,5 +1,16 @@
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faShoppingBag, faShoppingCart, faChevronCircleUp, faChevronCircleDown, faTimes, faTrash } from '@fortawesome/free-solid-svg-icons'
+import {
+  library
+} from '@fortawesome/fontawesome-svg-core'
+import {
+  faShoppingBag,
+  faShoppingCart,
+  faChevronCircleUp,
+  faChevronCircleDown,
+  faTimes,
+  faTrash,
+  faPlus,
+  faMinus
+} from '@fortawesome/free-solid-svg-icons'
 // import { faVuejs } from '@fortawesome/free-brands-svg-icons'
 
-library.add(faShoppingBag, faShoppingCart, faChevronCircleUp, faChevronCircleDown, faTimes, faTrash)
+library.add(faShoppingBag, faShoppingCart, faChevronCircleUp, faChevronCircleDown, faTimes, faTrash, faPlus, faMinus)
